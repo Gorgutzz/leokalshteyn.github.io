5147fbd0686b86f863bf45afe269a9bbbe043f37
leo <leo.kalshteyn@gmail.com>
2020-08-15 18:50:14 -0700

    import thumbnail with project links and titles in project component

--- src/Projects.js
+++ src/Projects.js
@@ -1,9 +1,28 @@
-import React from "react"
+import React from 'react';
+import Thumbnail from './Thumbnail.js';
+import './App.css';
 
 function Projects(props) {
   return (
     <div>
       <h1>Projects</h1>
+        <Thumbnail
+          link="https://github.com/Gorgutzz/museum-manager-rails"
+          image=""
+          title="Museum Manager"
+        />
+
+        <Thumbnail
+          link="https://github.com/Gorgutzz/planet-database-api"
+          image=""
+          title="Planet Database"
+        />
+
+        <Thumbnail
+          link="https://github.com/Gorgutzz/bb2-teams-app"
+          image=""
+          title="Blood Bowl 2 Teams"
+        />
     </div>
   )
 }
