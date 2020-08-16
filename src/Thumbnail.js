59c567bc48230c4b07fc437aa861512bb95c37fd
leo <leo.kalshteyn@gmail.com>
2020-08-15 18:53:04 -0700

    link project titles and image in thumbnail

--- src/Thumbnail.js
+++ src/Thumbnail.js
@@ -1,19 +1,18 @@
-import React from 'react'; 
+import React from 'react';
 import {Link} from 'react-router-dom';
 import './App.css';
 
 function Thumbnail(props) {
   return (
     <div className="project">
-      <Link to="">
+      <Link to={props.link}>
         <div className="project-image">
-          <img src="" alt="Project Image"/>
+          <img src={props.image} alt="Project Image"/>
         </div>
-        <div className="project-title">""</div>
-        <div className="project-category">""</div>
-      </Link>
+        <div className="project-title">{props.title}</div>
+            </Link>
     </div>
-  );
+  )
 }
 
 export default Thumbnail;
